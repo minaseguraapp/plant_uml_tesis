807cfef17d50fac48d8fe99e8a7c5b9a932ee393
Rosemberg Porras <rporras@consultec-ti.com>
2024-01-10 09:41:04 -0500

    fixed execute bat

--- MinaSegura/execute.bat
+++ MinaSegura/execute.bat
@@ -1,7 +1,3 @@
 @ECHO OFF
-<<<<<<< HEAD
 CD ..
 java -jar plantuml.jar "MinaSegura/src/**.puml" -o "../../output/"
-=======
-java -jar ../plantuml.jar src/**.puml -o "../../output"
->>>>>>> bf6ee1080e8a896d0a9c87de87aff3db806528e1
